91c8d1668a79ea7a290b56ff820484cc2a22f1b9
Anna Panferova <anyapanf@gmail.com>
2023-06-27 19:17:23 +0600

    pockemons finished

--- js/pockemon.js
+++ js/pockemon.js
@@ -1,31 +1,94 @@
 let wrapper = document.querySelector('.pockemon__wrapper')
+let pockeArr = []
+let modal = document.createElement('div')
 
 const getPockemon = async () => {
     let pocke = await fetch('https://pokeapi.co/api/v2/pokemon/')
     let data = await pocke.json()
     data.results.forEach((el, idx) => {
-        let card = document.createElement('div')
-        card.className = 'pockemon__card'
-        wrapper.appendChild(card)
+        const getInfo = async () => {
+            let pocke2 = await fetch(`${data.results[idx].url}`)
+            let data2 = await pocke2.json()
+            pockeArr = [...pockeArr, data2]
+            console.log(pockeArr);
 
-        let pockemonContent = document.createElement('div')
-        pockemonContent.className = 'pockemon__content'
-        card.appendChild(pockemonContent)
+            if (pockeArr.length === 20) {
+                pockeArr.forEach((el) => {
+                    let card = document.createElement('div')
+                    card.className = 'pockemon__card'
+                    wrapper.appendChild(card)
 
-        let pockemonName = document.createElement('h3')
-        pockemonName.className = 'pockemon__name'
-        pockemonContent.appendChild(pockemonName)
-        pockemonName.innerText = el.name
+                    let pockemonContent = document.createElement('div')
+                    pockemonContent.className = 'pockemon__content'
+                    card.appendChild(pockemonContent)
 
-        let pockemonImg = document.createElement('div')
-        pockemonImg.className = 'pockemon__img'
-        pockemonContent.appendChild(pockemonImg)
+                    let pockemonName = document.createElement('h3')
+                    pockemonName.className = 'pockemon__name'
+                    pockemonContent.appendChild(pockemonName)
+                    pockemonName.innerText = el.name
 
-        let img = document.createElement('img')
-        pockemonImg.appendChild(img)
-        img.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${idx + 1}.png`
+                    let pockeId = el.id
+
+                    let pockemonImg = document.createElement('div')
+                    pockemonImg.className = 'pockemon__img'
+                    pockemonContent.appendChild(pockemonImg)
+
+                    let img = document.createElement('img')
+                    pockemonImg.appendChild(img)
+                    img.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pockeId}.png`
+
+                    pockemonImg.addEventListener('click', () => {
+                        modal = document.createElement('div')
+                        wrapper.appendChild(modal)
+                        modal.className = 'pockemon__modal'
+
+                        let pockemonInfo = document.createElement('div')
+                        pockemonInfo.className = 'pockemon__info'
+                        modal.appendChild(pockemonInfo)
+
+                        let close = document.createElement('i')
+                        pockemonInfo.appendChild(close)
+                        close.className = 'pockemon__close fa-sharp fa-solid fa-circle-xmark fa-2xl'
+                        close.style.color = '#e10e0e'
+                        close.addEventListener('click', () => {
+                            modal.remove()
+                        })
+
+                        let pockemonPic = document.createElement('div')
+                        pockemonInfo.appendChild(pockemonPic)
+                        pockemonPic.className = 'pockemon__pic'
+
+                        let img = document.createElement('img')
+                        pockemonPic.appendChild(img)
+                        img.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pockeId}.png`
+
+                        let pockemonText = document.createElement('div')
+                        pockemonInfo.appendChild(pockemonText)
+                        pockemonText.className = 'pockemon__text'
+
+                        let title = document.createElement('h2')
+                        pockemonText.appendChild(title)
+                        title.className = 'pockemon__title'
+                        title.innerText = el.name
+                        let pokeExp = document.createElement("p")
+                        pockemonText.appendChild(pokeExp)
+                        pokeExp.className = "pockemon__experience"
+                        pokeExp.innerText = `Base Experience: ${el.base_experience}`
+                        el.abilities.forEach((el) => {
+                            let pokeAbility = document.createElement("p")
+                            pockemonText.appendChild(pokeAbility)
+                            pokeAbility.className = "pockemon__ability"
+                            pokeAbility.innerText = `Ability: ${el.ability.name}`
+                        })
+
+
+                    })
+
+                })
+            }
+        }
+        getInfo()
     })
-    console.log(data);
 }
 getPockemon()
 
